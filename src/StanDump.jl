f42679ecd9b31bb5269ef5f40c533682ec3816ca
Tamas K. Papp <tkpapp@gmail.com>
2019-01-06 11:16:59 +0100

    cleanup for Julia 1.0 (#1)
    cleanup for Julia 1.0, remove @vardict, allow NamedTuples

--- src/StanDump.jl
+++ src/StanDump.jl
@@ -1,15 +1,27 @@
+"""
+StanDump --- a package for writing data in the CmdStan dump data format.
+
+The single exported function is [`standump`](@ref).
+"""
 module StanDump
 
-using ArgCheck
+using ArgCheck: @argcheck
+using DocStringExtensions: SIGNATURES
 
-export StanDumpIO, standump, @vardict
+export standump
+
+####
+#### Representation of formatting
+####
 
 """
-Wrapper for an IO stream for writing data for use by Stan. See
-constructor for documentation of slots.
+Wrapper for an IO stream for writing data for use by Stan. See constructor for documentation
+of slots.
+
+Part of the API, but not exported.
 """
-struct StanDumpIO
-    io::IO
+struct StanDumpIO{T <: IO}
+    io::T
     def_arrow::Bool
     def_newline::Bool
     compact::Bool
@@ -21,137 +33,157 @@ end
 Wrap an IO stream `io` for writing data to be read by Stan.
 
 # Arguments
-* `def_arrow::Bool`: when `true` use `<-`, otherwise `=` for variable definitions.
-* `def_newline::Bool`: when `true`, `=` or `<-` is followed by a newline.
-* `compact::Bool`: when `true`, drop spaces when possible.
+
+- `def_arrow::Bool`: when `true` use `<-`, otherwise `=` for variable definitions.
+
+- `def_newline::Bool`: when `true`, each `=` or `<-` is followed by a newline.
+
+- `compact::Bool`: when `true`, drop whitespace when possible.
 """
 function StanDumpIO(io; def_arrow = true, def_newline = false, compact = false)
     StanDumpIO(io, def_arrow, def_newline, compact)
 end
 
-"""
-    standump(sd, xs...)
-
-Write arguments `xs...` as data for Stan into `sd`.
-
-Methods are only provided for objects which are valid. Use `varname =>
-value` (`Pair`) to represent assignments.
-"""
-standump(sd::StanDumpIO, xs...) = for x in xs standump(sd, x) end
-
-standump(sd::StanDumpIO, x) = error("Can't represent $x as data for Stan.")
+####
+#### Internals
+####
 
 """
-    _standump(sd, xs...)
+    dump(sd, xs...)
 
-Write arguments `xs...` as data for Stan into `sd`, passing through
-strings and characters, and allowing other special objects which are
-not valid data.
+Write arguments `xs...` as data for Stan into `sd`. For internal use.
 
-NOTE: For internal use. Define `standump` methods for valid Stan
-objects, and `_standump` methods for everything else, which are called
-by the former.
+NOTE: Define methods only for valid Stan objects, using `_dump` for everything else.
 """
-_standump(sd::StanDumpIO, xs...) = for x in xs _standump(sd, x) end
+dump(sd::StanDumpIO, x) = throw(ArgumentError("Can't represent $(x) as data for Stan."))
 
-_standump(sd::StanDumpIO, x) = standump(sd, x)
+dump(sd::StanDumpIO, x::Float64) = print(sd.io, x)
 
-_standump(sd::StanDumpIO, x::Union{Char,String}) = print(sd.io, x)
+dump(sd::StanDumpIO, x::Real) = dump(sd, Float64(x))
 
-"Write a space unless output is requested to be compact."
-struct CompactSpace end
-const COMPACTSPACE = CompactSpace()
-_standump(sd::StanDumpIO, ::CompactSpace) = if !sd.compact print(sd.io, " ") end
-
-function standump(sd::StanDumpIO, x::Integer)
+function dump(sd::StanDumpIO, x::Integer)
     if typemin(Int32) ≤ x ≤ typemax(Int32)
         print(sd.io, x)
     elseif typemin(Int64) ≤ x ≤ typemax(Int64)
         print(sd.io, x, "L")
     else
-        error("Integer too large to represent in Stan.")
+        throw(ArgumentError("Integer too large to represent in Stan."))
     end
 end
-        
-standump(sd::StanDumpIO, x::Real) = print(sd.io, Float64(x))
 
 """
-    is_valid_stan_varname(varname)
+$(SIGNATURES)
 
-Test if `varname` is valid as a Stan variable name.
+Write arguments arguments as data for Stan into `sd`, passing through strings and
+characters, and allowing other special objects which are not valid data.
+
+For internal implementation. also defined for objects which are not valid in Stan.
+"""
+_dump(sd::StanDumpIO, xs...) = for x in xs _dump(sd, x) end
+
+_dump(sd::StanDumpIO, x) = dump(sd, x)
+
+_dump(sd::StanDumpIO, x::Union{Char,String}) = print(sd.io, x)
+
+"Write a space unless output is requested to be compact."
+struct Space end
+
+const SPACE = Space()
+
+_dump(sd::StanDumpIO, ::Space) = sd.compact || print(sd.io, " ")
+
+"""
+$(SIGNATURES)
+
+Test if the argument is valid as a Stan variable name.
 
 NOTE: only basic checks, does not test conflicts with reserved names.
 """
-function is_valid_stan_varname(varname::String)
-    isvalid(c) = isascii(c) && (isalnum(c) || c == '_')
-    all(isvalid, varname) && isalpha(varname[1]) && !endswith(varname, "__")
+function is_valid_varname(name::String)
+    isvalid(c) = isascii(c) && (isdigit(c) || isletter(c) || c == '_')
+    all(isvalid, name) && isletter(name[1]) && !endswith(name, "__")
 end
 
-function _standump(sd::StanDumpIO, x::Symbol)
+function _dump(sd::StanDumpIO, x::Symbol)
     v = string(x)
-    @argcheck is_valid_stan_varname(v) "Invalid variable name $v."
+    @argcheck is_valid_stan_varname(v) "Invalid variable name $(v)."
     print(sd.io, v)
 end
 
-function standump{T}(sd::StanDumpIO, x::Pair{Symbol,T})
-    _standump(sd,
-              x.first, COMPACTSPACE,
-              sd.def_arrow ? "<-" : '=',
-              sd.def_newline ? "\n" : COMPACTSPACE,
-              x.second, "\n")
+function _dump(sd::StanDumpIO, x::Pair)
+    name, value = x
+    varname = string(name)
+    @argcheck is_valid_varname(varname)
+    _dump(sd, varname, SPACE, sd.def_arrow ? "<-" : '=',
+          sd.def_newline ? "\n" : SPACE, value, "\n")
 end
 
-function _standump_vector{T <: Real}(sd::StanDumpIO, xs::AbstractVector{T})
-    if isempty(xs)
-        _standump(sd, T <: Integer ? "integer" : "double", "(0)")
+"""
+$(SIGNATURES)
+
+Dump elements of a vector (or iterable, considered as a vector).
+
+When the element type is `<:Integer`, dump as `Int`s, otherwise as `Float64`; all values are
+converted for consistency.
+"""
+function _dump_vector(sd::StanDumpIO, x)
+    S = eltype(x) <: Integer ? Int : Float64
+    if isempty(x)
+        _dump(sd, S ≡ Int ? "integer" : "double", "(0)")
     else
-        _standump(sd, "c(")
-        for (i,x) in enumerate(xs)
-            if i > 1
-                _standump(sd, ",", COMPACTSPACE)
-            end
-            _standump(sd, x)
+        _dump(sd, "c(")
+        for (i, x) in enumerate(x)
+            i > 1 && _dump(sd, ",", SPACE)
+            dump(sd, S(x))      # convert value for consistency
         end
-        _standump(sd, ")")
+        _dump(sd, ")")
     end
 end
 
-standump{T <: Real}(sd::StanDumpIO, xs::AbstractVector{T}) =
-    _standump_vector(sd, xs)
+dump(sd::StanDumpIO, x::AbstractVector) = _dump_vector(sd, x)
 
-function standump{T <: Integer}(sd::StanDumpIO, r::UnitRange{T})
-    if r.start <= r.stop
-        _standump(sd, r.start, ":", r.stop)
-    else
-        _standump_vector(sd, r)
-    end
+function dump(sd::StanDumpIO, r::UnitRange{<: Integer})
+    isempty(r) ? _dump_vector(sd, r) : _dump(sd, minimum(r), ":", maximum(r))
 end
 
-function standump{T <: Real}(sd::StanDumpIO, A::AbstractArray{T})
-    _standump(sd, "structure(", view(A, :), ",", COMPACTSPACE,
-              ".Dim", COMPACTSPACE, "=", COMPACTSPACE, collect(size(A)), ")")
+function dump(sd::StanDumpIO, A::AbstractArray)
+    _dump(sd, "structure(", view(A, :), ",", SPACE, ".Dim", SPACE, "=",
+          SPACE, collect(size(A)), ")")
 end
 
-function standump{T}(sd::StanDumpIO, dict::Dict{Symbol, T})
-    for (varname, value) in dict
-        standump(sd, varname => value)
-    end
-end
-
-function standump{T}(io::IO, dict::Dict{Symbol, T}; options...)
-    standump(StanDumpIO(io; options...), dict)
-end
+####
+#### Interface
+####
 
 """
-    @vardict varnames...
+    standump(filename, data; force = false, kwargs...)
+    standump(io, data; kwargs...)
+    standump(StanDump.StanDumpIO(io; kwargs...), data)
 
-Create a `Dict` of `:varname => varname` values. Useful as a shorthand
-for creating dictionaries of variables and their values before passing
-them to `standump`.
+Write `data`, which is a value that supports `pairs` (eg a `NamedTuple` or a `Dict`) to
+`filename` or `io`.
+
+Using a `filename`, it will not be overwritten unless `force = true` is specified.
+
+Keyword arguments are passed to `StanDumpIO` to govern formatting (most users should not
+care about this, except for debugging purposes).
+
+# Example
+
+```jldoctest
+julia> standump(stdout, (N = 1, a = 1:5, b = ones(2, 2)))
+N <- 1
+a <- 1:5
+b <- structure(c(1.0, 1.0, 1.0, 1.0), .Dim = c(2, 2))
+```
 """
-macro vardict(varnames::Symbol...)
-    args = [:($(Meta.quot(varname)) => $(esc(varname))) for varname in varnames]
-    :(Dict($(args...)))
+standump(sd::StanDumpIO, data) = foreach(p -> _dump(sd, p), pairs(data))
+
+standump(io::IO, data; kwargs...) = standump(StanDumpIO(io; kwargs...), data)
+
+function standump(filename::AbstractString, data; force::Bool = false, kwargs...)
+    @argcheck force || !ispath(filename) "$(filename) already exists"
+    open(io -> standump(StanDumpIO(io; kwargs...), data), filename, "w")
 end
 
 end # module
